92c911b1454daa327fefeb2aed1e2248940d7541
RishaanJ <rishaanjain@gmail.com>
2024-07-10 22:15:27 -0700

    kS and kV values + robo centric teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -66,8 +66,8 @@ public final class MecanumDrive {
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 1.4164468812596152;
+        public double kV = 0.00009809902677246193;
         public double kA = 0;
 
         // path profile parameters (in inches)
